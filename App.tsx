d43de5af1c610c1e7aeb85e4db2fb527f30994e7
davutbilgicnl <davutbilgic.nl@gmail.com>
2024-04-05 20:21:40 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { ThemeProvider } from './Contexts/ThemeContext';
+import { ThemeContext, ThemeProvider } from './Contexts/ThemeContext';
+import { useContext } from 'react';
 
 const MainApp: React.FC = () => {
+  const { theme } = useContext(ThemeContext);
+
+  const styles = getStyles(theme);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -11,14 +16,16 @@ const MainApp: React.FC = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const getStyles = (theme: any) => {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.background,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+};
 
 const App: React.FC = () => {
   return (
