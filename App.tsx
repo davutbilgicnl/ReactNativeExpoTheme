007d7d621f7c6f132ea163d23bc3b29cc3916077
davutbilgicnl <davutbilgic.nl@gmail.com>
2024-04-05 20:14:09 +0200

    Add ThemeProvider to App component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from './Contexts/ThemeContext';
 
-export default function App() {
+const MainApp: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +19,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const App: React.FC = () => {
+  return (
+    <ThemeProvider>
+      <MainApp />
+    </ThemeProvider>
+  );
+};
