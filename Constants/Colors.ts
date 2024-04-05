3ce0540424d7dbcd327beab26dc1268599aeabe0
davutbilgicnl <davutbilgic.nl@gmail.com>
2024-04-05 20:06:45 +0200

    Constants>Colors.ts

---
+++ Constants/Colors.ts
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  primary: '#6200ee',
+  background: '#fffae3',
+  text: '#000000',
+  secondary: '#03dac6',
+};
+
+export const darkTheme = {
+  primary: '#bb86fc',
+  background: '#121212',
+  text: '#ffffff',
+  secondary: '#03dac6',
+};
